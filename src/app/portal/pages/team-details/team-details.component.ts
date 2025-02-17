1c80e2447fe1cff2d4b2decbbbc9da7368f4b8ef
mahi-bashar-akash <mahibashar2023@gmail.com>
2025-02-17 20:03:58 +0600

    as-progress-coding-in-interface

--- src/app/portal/pages/team-details/team-details.component.ts
+++ src/app/portal/pages/team-details/team-details.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-team-details',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './team-details.component.html',
   styleUrl: './team-details.component.css'
 })
